94884b2d8f9ad6781423da83458d9b46c0548afa
Meghana-tanikonda <106367311+Meghana-tanikonda@users.noreply.github.com>
2025-04-04 12:36:14 +0530

    Update Program.cs

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,52 +1,17 @@
-﻿using System;
-using System.ComponentModel;
-namespace college
+using System;
+namespace MyNamespace
 {
-
-    class Student
+    class MyClass
     {
-        int x = 12;
-        int y = 12;
-        public int add(int a,int b)//and method with return type and with parmeters
+        static void add(int x, int y)//static method adding two numbers
         {
-            int c = a + b;
-            return c;
+            int z = x + y;
+            Console.WriteLine($"Adding two numbers is {z}");
         }
-        public void sub(int d, int e)//method with parameters(datatypes along with names), without return type
-        { 
-            int f = d - e;
-            Console.WriteLine($"Sub of two numbers {f}");
-        }
-        public void mul()//method without return type and parameters
+        static void Main()
         {
-            int z = x * y;
-            Console.WriteLine($"Mul of two numbers {z}");
+            add(2, 3);
         }
-        public void div()
-        {
-            int z = x / y;
-            Console.WriteLine($"Division of two numbers is {z}");
-        }
-        public void mod()
-        {
-            int z = x % y;
-            Console.WriteLine($"Modules of two numbers is {z}");
-        }
-
-        static void Main(string[] args)
-        {
-            Student s = new Student();
-            int h = s.add(1,2);
-            s.sub(6, 2);
-            s.mul();
-            s.div();
-            s.mod();
-            Console.WriteLine($"Addition of two numbers is {h}");
-            Console.ReadLine();
-        }
-        
-      
-
     }
 }
 
